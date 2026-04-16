10e527c1fb2393ad65ce3e2a0fc174a863af8423
Matt Fiddaman <github@m.fiddaman.uk>
2026-04-16 21:42:27 +0100

    replace uuid and fs-extra with builtins (#7529)
    * fs-extra
    
    * uuid
    
    * note

--- packages/desktop-client/src/components/accounts/Account.tsx
+++ packages/desktop-client/src/components/accounts/Account.tsx
@@ -41,7 +41,6 @@ import type {
 import { t } from 'i18next';
 import debounce from 'lodash/debounce';
 import isEqual from 'lodash/isEqual';
-import { v4 as uuidv4 } from 'uuid';
 
 import {
   useReopenAccountMutation,
@@ -1304,7 +1303,7 @@ class AccountInternal extends PureComponent<
 
     const [firstTransaction] = transactions;
     const parentTransaction = {
-      id: uuidv4(),
+      id: crypto.randomUUID(),
       is_parent: true,
       cleared: transactions.every(t => !!t.cleared),
       date: firstTransaction.date,
